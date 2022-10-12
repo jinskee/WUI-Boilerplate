cddc6e4a2b49838ffeffdc2f18f075b61e75d14b
jinskee <98485932+jinskee@users.noreply.github.com>
2022-10-11 22:06:52 -0500

    Progress on Project

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ var waterFall;
 
 var wuiMc;
 
+var bridgerFoothills;
+
+var breachieCreek
+
 
 //function to instantiate the leaflet map
 function createMap(){
@@ -37,6 +41,8 @@ var Stadia_AlidadeSmooth = L.tileLayer('https://api.mapbox.com/styles/v1/jinskee
 getData(); 
 getDataTwo();
 getDataThree();//COMMMMMMMMEINTED OUT
+getDataFour(); 
+getDataFive();
 };
 
 
@@ -54,6 +60,7 @@ function getData(){   //Put map in paranthesis?
                     var popupContentCC = createPopupContentCC(feature);
                     layer.bindPopup(popupContentCC)
                 }
+                
 
             }).addTo(map)
 
@@ -111,6 +118,55 @@ function getDataThree(){   //Put map in paranthesis?
         }) 
 };
 
+function getDataFour(){   //Put map in paranthesis?
+    //load the data
+    fetch("data/bridgerFoothill.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            bridgerFoothills= L.geoJson(json,{
+                onEachFeature:function(feature, layer){
+                    var popupContentBFH = createPopupContentBFH(feature);
+                    layer.bindPopup(popupContentBFH)
+                }
+            }).addTo(map)
+
+            //var attributes = processData(json);
+            //calcStats(json)
+            //create marker options
+            //callfunction to create proportional symbols
+            //createPropSymbols(json, attributes);
+            //createSequenceControls(attributes);
+            //createLegend(attributes);
+        }) 
+};
+
+function getDataFive(){   //Put map in paranthesis?
+    //load the data
+    fetch("data/breachieCreek.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            breachieCreek= L.geoJson(json,{
+                onEachFeature:function(feature, layer){
+                    var popupContentBCF = createPopupContentBCF(feature);
+                    layer.bindPopup(popupContentBCF)
+                }
+                
+            }).addTo(map)
+
+            //var attributes = processData(json);
+            //calcStats(json)
+            //create marker options
+            //callfunction to create proportional symbols
+            //createPropSymbols(json, attributes);
+            //createSequenceControls(attributes);
+            //createLegend(attributes);
+        }) 
+};
+
 function createPopupContent(feature){
     var popupContent = "<p><b>Fire:</b> " + feature.properties.INCDNT_NM +
         "</p><p><b>Year:</b> " +feature.properties.Year +
@@ -123,5 +179,23 @@ function createPopupContentCC(feature){
     var popupContentCC = "<p><b>Area:</b> " + feature.properties.name +
         "</p><p><b>Acres of WUI:</b> " + feature.properties.Acres;
     return popupContentCC
-}
+};
+function createPopupContentBFH(feature){
+    var popupContentBFH = "<p><b>Fire:</b> " + feature.properties.FIRENAME +
+    "</p><p><b>Year:</b> " +feature.properties.FIREYEAR +
+    "</p><p><b>Total Acres:</b> " + feature.properties.TOTALACRES +
+    "</p><p><b>Structures Lost:</b> " + feature.properties.homes +
+    "</p><p><b>Cause:</b> " + feature.properties.cause;
+return popupContentBFH
+};
+
+function createPopupContentBCF(feature){
+    var popupContentBCF = "<p><b>Fire:</b> " + feature.properties.FIRENAME +
+    "</p><p><b>Year:</b> " +feature.properties.FIREYEAR +
+    "</p><p><b>Total Acres:</b> " + feature.properties.TOTALACRES +
+    "</p><p><b>Structures Lost:</b> " + feature.properties.homes +
+    "</p><p><b>Cause:</b> " + feature.properties.cause;
+return popupContentBCF
+};
+
 document.addEventListener('DOMContentLoaded', createMap)
